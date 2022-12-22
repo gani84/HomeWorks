7eb360c1e35dcbabf1c5a2957f4bd6624a1b61c4
gani <gani84@mail.ru>
2022-12-22 20:15:36 +0600

    First lesson

---
+++ Seminar1/Program.cs
@@ -0,0 +1,50 @@
+﻿/*
+// Задача 1
+Console.WriteLine("Введите переменную a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите переменную b");
+int b = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+
+Console.Write("max = ");
+Console.WriteLine(max); */
+
+/*
+// Задача 2
+Console.WriteLine("Введите переменную a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите переменную b");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите переменную c");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max); */
+
+/* // Задача 3
+Console.WriteLine("Введите переменную a");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2==0)
+            {
+                Console.WriteLine("число четное");
+           
+            }
+            else
+            {
+                Console.WriteLine("Число нечетное");
+            }
+*/   
+
+/* // Задача 4
+Console.WriteLine("Введите переменную N");
+int n = Convert.ToInt32(Console.ReadLine());         
+Console.WriteLine();
+for (int i = 1; i <= n; i++)
+if (i % 2==0)
+Console.Write(i + " "); */
